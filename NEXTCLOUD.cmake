usatiuk/nextcloud-desktop

Implement URI handler for local file edit
Signed-off-by: alex-z <blackslayer4@gmail.com>

NEXTCLOUD.cmake
@@ -5,6 +5,7 @@ set( APPLICATION_DOMAIN     "nextcloud.com" )
 set( APPLICATION_VENDOR     "Nextcloud GmbH" )
 set( APPLICATION_UPDATE_URL "https://updates.nextcloud.org/client/" CACHE STRING "URL for updater" )
 set( APPLICATION_HELP_URL   "" CACHE STRING "URL for the help menu" )
+set( APPLICATION_COMMAND_OPEN_URL_SCHEME "nc")
 
 if(APPLE AND APPLICATION_NAME STREQUAL "Nextcloud" AND EXISTS "${CMAKE_SOURCE_DIR}/theme/colored/Nextcloud-macOS-icon.svg")
     set( APPLICATION_ICON_NAME "Nextcloud-macOS" )

src/common/utility_unix.cpp
@@ -19,6 +19,7 @@
 
 #include <QStandardPaths>
 #include <QtGlobal>
+#include <QProcess>
 
 namespace OCC {
 
@@ -111,6 +112,27 @@ static inline bool hasDarkSystray_private()
 QString Utility::getCurrentUserName()
 {
     return {};
+}
+
+void Utility::registerUriHandler()
+{
+    const auto appImagePath = qEnvironmentVariable("APPIMAGE");
+    const auto runningInsideAppImage = !appImagePath.isNull() && QFile::exists(appImagePath);
+
+    if (runningInsideAppImage) {
+        // only register x-scheme-handler if running inside appImage
+        return;
+    }
+
+    const QString desktopFileName = QLatin1String(LINUX_APPLICATION_ID) + QLatin1String(".desktop");
+    QProcess process;
+    const QStringList args = {
+        QLatin1String("default"),
+        desktopFileName,
+        QStringLiteral("x-scheme-handler/%1").arg(QStringLiteral(APPLICATION_COMMAND_OPEN_URL_SCHEME))
+    };
+    process.start(QStringLiteral("xdg-mime"), args, QIODevice::ReadOnly);
+    process.waitForFinished();
 }
 
 } // namespace OCC

src/gui/application.cpp
@@ -402,6 +402,8 @@ Application::Application(int &argc, char **argv)
     connect(_gui.data(), &ownCloudGui::isShowingSettingsDialog, this, &Application::slotGuiIsShowingSettings);
 
     _gui->createTray();
+
+    parseEditLocalFileArgument();
 }
 
 Application::~Application()
@@ -568,6 +570,8 @@ void Application::slotParseMessage(const QString &msg, QObject *)
             qApp->quit();
         }
 
+        parseEditLocalFileArgument();
+
     } else if (msg.startsWith(QLatin1String("MSG_SHOWMAINDIALOG"))) {
         qCInfo(lcApplication) << "Running for" << _startedAt.elapsed() / 1000.0 << "sec";
         if (_startedAt.elapsed() < 10 * 1000) {
@@ -643,7 +647,19 @@ void Application::parseOptions(const QStringList &options)
         } else if (option.endsWith(QStringLiteral(APPLICATION_DOTVIRTUALFILE_SUFFIX))) {
             // virtual file, open it after the Folder were created (if the app is not terminated)
             QTimer::singleShot(0, this, [this, option] { openVirtualFile(option); });
-        } else {
+        } else if (option.startsWith(APPLICATION_COMMAND_OPEN_URL_SCHEME"://open")) {
+            // the URL must be 'APPLICATION_COMMAND_OPEN_URL_SCHEME://open/username@example.com<optional port number :8080>/relative-path-to-file'
+            // e.g. nc://open/admin@nextcloud.lan:8080/Photos/lovely.jpg
+            // e.g. nc://open/user@example.de/Photos/lovely.jpg
+            _editFileLocallyUrl = QUrl::fromUserInput(option);
+            if (!_editFileLocallyUrl.isValid()) {
+                _editFileLocallyUrl.clear();
+                const auto errorParsingLocalFileEditingUrl = QString("The supplied url for local file editing '%1' is invalid!").arg(option);
+                qCInfo(lcApplication) << errorParsingLocalFileEditingUrl;
+                showHint(errorParsingLocalFileEditingUrl.toStdString());
+            }
+        }
+        else {
             showHint("Unrecognized option '" + option.toStdString() + "'");
         }
     }
@@ -722,6 +738,26 @@ bool Application::backgroundMode() const
 void Application::setHelp()
 {
     _helpOnly = true;
+}
+
+void Application::parseEditLocalFileArgument()
+{
+    if (!_editFileLocallyUrl.isValid()) {
+        return;
+    }
+
+    auto parameters = _editFileLocallyUrl.path().split('/', Qt::SkipEmptyParts);
+    _editFileLocallyUrl.clear();
+
+    if (parameters.size() < 2) {
+        qCWarning(lcApplication) << "Invalid URL for file local editing: " + parameters.join('/');
+        return;
+    }
+
+    const auto accountDisplayName = parameters.takeFirst();
+    const auto fileRemotePath = parameters.join('/');
+
+    FolderMan::instance()->editFileLocally(accountDisplayName, fileRemotePath);
 }
 
 QString substLang(const QString &lang)

BMerz/desktop

NEXTCLOUD.cmake
@@ -5,6 +5,7 @@ set( APPLICATION_DOMAIN     "nextcloud.com" )
 set( APPLICATION_VENDOR     "Nextcloud GmbH" )
 set( APPLICATION_UPDATE_URL "https://updates.nextcloud.org/client/" CACHE STRING "URL for updater" )
 set( APPLICATION_HELP_URL   "" CACHE STRING "URL for the help menu" )
+set( APPLICATION_URI_HANDLER_SCHEME "nc")
 
 if(APPLE AND APPLICATION_NAME STREQUAL "Nextcloud" AND EXISTS "${CMAKE_SOURCE_DIR}/theme/colored/Nextcloud-macOS-icon.svg")
     set( APPLICATION_ICON_NAME "Nextcloud-macOS" )

src/common/utility_unix.cpp
@@ -19,6 +19,7 @@
 
 #include <QStandardPaths>
 #include <QtGlobal>
+#include <QProcess>
 
 namespace OCC {
 
@@ -111,6 +112,28 @@ static inline bool hasDarkSystray_private()
 QString Utility::getCurrentUserName()
 {
     return {};
+}
+
+void Utility::registerUriHandlerForLocalEditing()
+{
+    const auto appImagePath = qEnvironmentVariable("APPIMAGE");
+    const auto runningInsideAppImage = !appImagePath.isNull() && QFile::exists(appImagePath);
+
+    if (runningInsideAppImage) {
+        // only register x-scheme-handler if running inside appImage
+        return;
+    }
+
+    // mirall.desktop.in must have an x-scheme-handler mime type specified
+    const QString desktopFileName = QLatin1String(LINUX_APPLICATION_ID) + QLatin1String(".desktop");
+    QProcess process;
+    const QStringList args = {
+        QLatin1String("default"),
+        desktopFileName,
+        QStringLiteral("x-scheme-handler/%1").arg(QStringLiteral(APPLICATION_URI_HANDLER_SCHEME))
+    };
+    process.start(QStringLiteral("xdg-mime"), args, QIODevice::ReadOnly);
+    process.waitForFinished();
 }
 
 } // namespace OCC

src/gui/application.cpp
@@ -402,6 +402,8 @@ Application::Application(int &argc, char **argv)
     connect(_gui.data(), &ownCloudGui::isShowingSettingsDialog, this, &Application::slotGuiIsShowingSettings);
 
     _gui->createTray();
+
+    parseEditLocalFileArgument();
 }
 
 Application::~Application()
@@ -568,6 +570,8 @@ void Application::slotParseMessage(const QString &msg, QObject *)
             qApp->quit();
         }
 
+        parseEditLocalFileArgument();
+
     } else if (msg.startsWith(QLatin1String("MSG_SHOWMAINDIALOG"))) {
         qCInfo(lcApplication) << "Running for" << _startedAt.elapsed() / 1000.0 << "sec";
         if (_startedAt.elapsed() < 10 * 1000) {
@@ -643,7 +647,19 @@ void Application::parseOptions(const QStringList &options)
         } else if (option.endsWith(QStringLiteral(APPLICATION_DOTVIRTUALFILE_SUFFIX))) {
             // virtual file, open it after the Folder were created (if the app is not terminated)
             QTimer::singleShot(0, this, [this, option] { openVirtualFile(option); });
-        } else {
+        } else if (option.startsWith(APPLICATION_URI_HANDLER_SCHEME"://open")) {
+            // the URL must be 'APPLICATION_URI_HANDLER_SCHEME://open/username@example.com<optional port number :8080>/relative-path-to-file'
+            // e.g. nc://open/admin@nextcloud.lan:8080/Photos/lovely.jpg
+            // e.g. nc://open/user@example.de/Photos/lovely.jpg
+            _editFileLocallyUrl = QUrl::fromUserInput(option);
+            if (!_editFileLocallyUrl.isValid()) {
+                _editFileLocallyUrl.clear();
+                const QString errorParsingLocalFileEditingUrl = QStringLiteral("The supplied url for local file editing '%1' is invalid!").arg(option);
+                qCInfo(lcApplication) << errorParsingLocalFileEditingUrl;
+                showHint(errorParsingLocalFileEditingUrl.toStdString());
+            }
+        }
+        else {
             showHint("Unrecognized option '" + option.toStdString() + "'");
         }
     }
@@ -722,6 +738,26 @@ bool Application::backgroundMode() const
 void Application::setHelp()
 {
     _helpOnly = true;
+}
+
+void Application::parseEditLocalFileArgument()
+{
+    if (!_editFileLocallyUrl.isValid()) {
+        return;
+    }
+
+    auto parameters = _editFileLocallyUrl.path().split('/', Qt::SkipEmptyParts);
+    _editFileLocallyUrl.clear();
+
+    if (parameters.size() < 2) {
+        qCWarning(lcApplication) << "Invalid URL for file local editing: " + parameters.join('/');
+        return;
+    }
+
+    const auto accountDisplayName = parameters.takeFirst();
+    const auto fileRemotePath = parameters.join('/');
+
+    FolderMan::instance()->editFileLocally(accountDisplayName, fileRemotePath);
 }
 
 QString substLang(const QString &lang)